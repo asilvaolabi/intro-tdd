30ef3e7da055f00743a86e91ad3105ef64fab5e9
asilvaolabi <asilva@olabi.org.br>
2024-01-22 18:07:06 -0300

    Criando a interface da calculadora

--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -1,6 +1,7 @@
 const calculadora = require('./calculadora');
 
 test('calculaSalario deve retornar o salário correto', () => {
-  const salarioCalculado = calculadora.calculaSalario(40, 20); // 40 horas, R$20/hora
-  expect(salarioCalculado).toBe(800); // 40 * 20 = 800
+  const salarioCalculado = calculadora.calculaSalario(40, 20); 
+  expect(salarioCalculado).toBe(900); 
 });
+
